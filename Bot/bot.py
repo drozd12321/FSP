65f0f699cc98a10c7f3c8631a2b74ff78d182e51
Radmir <radmir.mannanov@bk.ru>
2025-04-22 17:35:04 +0300

    update_expert_system

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,3 +1,5 @@
+import re
+from datetime import datetime
 import asyncio
 import asyncpg
 from telegram import ReplyKeyboardMarkup, Update, ReplyKeyboardRemove
@@ -21,9 +23,9 @@ async def find_competitions_start(update: Update, context: ContextTypes.DEFAULT_
     context.user_data['answers'] = {}
     
     keyboard = [
-        ["🧑‍💻 Продуктовое программирование", "🛡️ Программирование систем ИБ"],
-        ["🤖 Программирование робототехники", "🧠 Алгоритмическое программирование"],
-        ["✈️ Программирование беспилотных авиационных систем"]
+        ["🧑‍💻 Продуктовое программирование", "🛡️ Программирование систем информационной безопасности"],
+        ["🤖 Программирование робототехники", "🧠 Программирование алгоритмическое"],
+        ["✈️ Программирование БАС"]
     ]
     await update.message.reply_text(
         "🏆 Давай найдем подходящие соревнования!\n"
@@ -60,7 +62,7 @@ async def handle_question_3(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     # Всегда переходим к вопросу о возрасте, независимо от выбора
     await update.message.reply_text(
-        "Укажи свой возраст:",
+        "Укажите свой возраст:",
         reply_markup=ReplyKeyboardRemove()
     )
     return QUESTION_4
@@ -80,7 +82,7 @@ async def handle_question_4(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # Добавляем новый вопрос об уровне подготовки
     keyboard = [["Индивидуальные", "Командные"]]
     await update.message.reply_text(
-        "Выбери тип соревнований",
+        "Выберите тип соревнований",
         reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     )
     return QUESTION_5  # Переходим к новому вопросу
@@ -91,7 +93,7 @@ async def handle_question_5(update: Update, context: ContextTypes.DEFAULT_TYPE):
     
     # Если выбраны региональные, спрашиваем регион
     if context.user_data['answers'].get('scale') == "Региональные":
-        await update.message.reply_text("Укажи, из какого ты региона:")
+        await update.message.reply_text("Укажите, из какого вы региона:")
         return QUESTION_6
     
     # Иначе завершаем опрос
@@ -103,17 +105,17 @@ async def handle_question_6(update: Update, context: ContextTypes.DEFAULT_TYPE):
     conn = await asyncpg.connect(**DB_CONFIG)
     try:
         # Здесь ваша логика запроса к базе данных
-        competitions = await conn.fetch(
+        reg_ = await conn.fetch(
             "SELECT * FROM win_region WHERE name = $1", 
             region
         )
-        print(competitions)
-        if competitions:
+        print(reg_)
+        if reg_:
             context.user_data['answers']['region'] = update.message.text
             return await finish_questionnaire(update, context)
         else:
             await update.message.reply_text("Вы ввели неверное название региона.\nВведите заново (например: Республика Татарстан):")
-            return await QUESTION_6
+            return QUESTION_6
     finally:
         await conn.close()
     # context.user_data['answers']['region'] = update.message.text
@@ -122,7 +124,21 @@ async def handle_question_6(update: Update, context: ContextTypes.DEFAULT_TYPE):
 async def finish_questionnaire(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Завершение опроса и вывод результатов"""
     answers = context.user_data['answers']
-    
+    discipline = cleaned_text = re.sub(r'[^\w\s]', '', answers['discipline']).strip()
+    format = answers['format']
+    if format == 'Онлайн':
+        format = "('online')"
+    elif format == 'Офлайн':
+        format = "('offline')"
+    else:
+        format = "('online','offline')"
+    scale = answers['scale']
+    age = int(answers['age'])
+    type_ = answers['type']
+    if type_ == 'Командные':
+        type_='team'
+    else:
+        type_='individual'
     report = (
         "📋\n"
         f"• Дисциплина: {answers.get('discipline', 'Не указано')}\n"
@@ -143,6 +159,107 @@ async def finish_questionnaire(update: Update, context: ContextTypes.DEFAULT_TYP
         f"{report}\n\nИщем подходящие варианты...",
         reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     )
+    print(discipline,format,int(age),type_)
+    print('гружу')
+    conn = await asyncpg.connect(**DB_CONFIG)
+    print('загрузил')
+    print(discipline)
+    try:
+        if 'region' in answers:
+            region = answers['region']
+            search_comp = await conn.fetch(
+            f'''
+    with comp as
+    (SELECT wc.id,
+    wc.max_participants, wc.max_participants_in_team,
+    wc.min_age, wc.max_age, wc."name", wc.competition_type,
+    wc.status, wc.description, wc."type", wc.permissions, wd."name" AS discipline,
+	win_competitiondate.start_date, win_competitiondate.end_date,
+	win_competitiondate.registration_start, win_competitiondate.registration_end
+    FROM win_competition wc
+    LEFT JOIN win_discipline wd
+    ON wc.discipline_id = wd.id
+    LEFT JOIN win_competitiondate
+    ON wc.id = win_competitiondate.competition_id)   
+    SELECT 
+    c.max_participants, c.max_participants_in_team,
+    c.min_age, c.max_age, c.name, c.competition_type,
+    c.status, c.description, c."type", c.discipline,
+    c.start_date, c.end_date, c.registration_start, c.registration_end,
+    elem->>'name' AS city_name
+    FROM comp c, jsonb_array_elements(c.permissions)AS elem
+    WHERE elem->>'name' = $1 and c.discipline = $2 and c.competition_type IN {format} and c.min_age<=$3 and c.type = $4; 
+        ''', 
+            region, discipline, age, type_
+            )
+            if len(search_comp)!=0:
+                for competition in search_comp:
+                    print(type(competition['start_date']))
+                    start_date = competition['start_date'].strftime("%d.%m.%Y %H:%M")
+                    end_date = competition['end_date'].strftime("%d.%m.%Y %H:%M")
+                    registration_start = competition['registration_start'].strftime("%d.%m.%Y %H:%M")
+                    registration_end = competition['registration_end'].strftime("%d.%m.%Y %H:%M")
+                    message = f'''Название:\n{competition['name']}
+Описание:\n{competition['description']}
+Дата проведения: {start_date} - {end_date}
+Регистрация: {registration_start} - {registration_end}
+    '''
+                    if type_=='team':
+                        message+=f'\nСостав команды - {competition['max_participants_in_team']} человек'
+                    message+='\nСсылка ...'
+                    await update.message.reply_text(message,reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True))
+            else:
+                await update.message.reply_text('По вашему запросу соревнования не найдены.',reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True))
+        else:
+            print(format)
+            search_comp = await conn.fetch(
+            f'''
+    with comp as
+    (SELECT wc.id,
+    wc.max_participants, wc.max_participants_in_team,
+    wc.min_age, wc.max_age, wc."name", wc.competition_type,
+    wc.status, wc.description, wc."type", wc.permissions, wd."name" AS discipline,
+	win_competitiondate.start_date, win_competitiondate.end_date,
+	win_competitiondate.registration_start, win_competitiondate.registration_end
+    FROM win_competition wc
+    LEFT JOIN win_discipline wd
+    ON wc.discipline_id = wd.id
+    LEFT JOIN win_competitiondate
+    ON wc.id = win_competitiondate.competition_id)
+    SELECT 
+    c.max_participants, c.max_participants_in_team,
+    c.min_age, c.max_age, c.name, c.competition_type,
+    c.status, c.description, c."type", c.discipline, c.permissions,
+    c.start_date, c.end_date, c.registration_start, c.registration_end
+    FROM comp c
+    WHERE 
+    c.permissions = '[]'::jsonb 
+    AND c.discipline = $1 
+    AND c.competition_type IN {format} 
+    AND c.min_age <= $2 
+    AND c.type = $3; 
+            ''', 
+            discipline, age, type_
+            )
+            if len(search_comp)!=0:
+                for competition in search_comp:
+                    start_date = competition['start_date'].strftime("%d.%m.%Y %H:%M")
+                    end_date = competition['end_date'].strftime("%d.%m.%Y %H:%M")
+                    registration_start = competition['registration_start'].strftime("%d.%m.%Y %H:%M")
+                    registration_end = competition['registration_end'].strftime("%d.%m.%Y %H:%M")
+                    message = f'''Название:\n{competition['name']}
+Описание:\n{competition['description']}
+Дата проведения: {start_date} - {end_date}
+Регистрация: {registration_start} - {registration_end}
+    '''
+                    if type_=='team':
+                        message+=f'\nКличество человек в команде - {competition['max_participants_in_team']}'
+                    message+='\nСсылка ...'
+                    await update.message.reply_text(message,reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True))
+            else:
+                await update.message.reply_text('По вашему запросу соревнования не найдены.',reply_markup=ReplyKeyboardMarkup(keyboard, resize_keyboard=True))
+    finally:
+        await conn.close()
     return ConversationHandler.END
 
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -242,7 +359,7 @@ async def handle_competitions(update: Update, context: ContextTypes.DEFAULT_TYPE
             else:
                 response = "🤷 Ты пока не участвуешь ни в каких соревнованиях.\nСкорее заходи на сайт и учавствуй!\nhttp://10.8.0.6:5173/"
         else:
-            response = "🔍 Не удалось найти твой аккаунт в системе"
+            response = "🔍 Не удалось найти твой аккаунт в системе. Выполни команду /start"
         
         await loading_msg.edit_text(response)
         
